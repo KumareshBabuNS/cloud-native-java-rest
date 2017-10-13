7b3b0b5a5258d975bc5683fedb190b7f600f650a
Juan Manuel Alvarez Rodriguez <jalvarro@usersad.everis.int>
2017-10-13 13:02:18 +0200

    Initial commit

---
+++ src/main/java/com/aljumaro/test/cloudnativejavarest/CloudNativeJavaRestApplication.java
@@ -0,0 +1,12 @@
+package com.aljumaro.test.cloudnativejavarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeJavaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeJavaRestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aljumaro/test/cloudnativejavarest/CloudNativeJavaRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aljumaro.test.cloudnativejavarest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudNativeJavaRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
